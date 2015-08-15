b14b316550140dbdd98118e069d4af8e6ad10ca5
Ethan Aubuchon <ethan.a.aubuchon@gmail.com>
2015-08-15 16:11:06 -0400

    Code cleaning

--- scraper.js
+++ scraper.js
@@ -11,14 +11,14 @@ var fetchData = function (url) {
 
 var objToString = function(obj) {
     return obj.toString();
-}
+};
 
 var writeToFile = function (file, data) {
     fs.writeFile(file, JSON.stringify(data), function (err) {
         if (err) return console.log(err);
         console.log(file, "written");
     });
-}
+};
 
 var teamsToJSON = function ($) {
     var teams = [];
